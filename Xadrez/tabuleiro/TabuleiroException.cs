a708abb6afb90ddf60220387f3ae3337c561e595
David Moraes <InvesTraderRS@gmail.com>
2020-09-25 16:00:08 -0300

    Tratando erros com Excecao personalizada

---
+++ Xadrez/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Xadrez.tabuleiro
+{
+    class TabuleiroException : Exception
+    {
+        public TabuleiroException(string msg) : base(msg)
+        {
+        }
+    }
+}
